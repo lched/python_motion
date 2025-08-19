ecc9a23418204d9e5d0e3e67b7313259cd5335d1
lched <leo.chedin@ens-paris-saclay.fr>
2025-08-19 16:27:36 +0200

    Change smpl_to_bvh_data to not use SMPLX bust fixed SMPL offsets instead

--- Motion/smpl.py
+++ Motion/smpl.py
@@ -1,9 +1,7 @@
 # From https://github.com/KosukeFukazawa/smpl2bvh/blob/main/smpl2bvh.py
-from pathlib import Path
 import pickle
 
 import numpy as np
-import smplx
 
 # import torch
 
@@ -38,6 +36,60 @@ SMPL_JOINTS_NAMES = [
     "R_Wrist",
     "L_Hand",
     "R_Hand",
+]
+
+SMPL_OFFSETS = [
+    [0.0, 0.0, 0.0],
+    [0.05858135, -0.08228004, -0.01766408],
+    [-0.06030973, -0.09051332, -0.01354254],
+    [0.00443945, 0.12440352, -0.03838522],
+    [0.04345142, -0.38646945, 0.008037],
+    [-0.04325663, -0.38368791, -0.00484304],
+    [0.00448844, 0.1379564, 0.02682033],
+    [-0.01479032, -0.42687458, -0.037428],
+    [0.01905555, -0.4200455, -0.03456167],
+    [-0.00226458, 0.05603239, 0.00285505],
+    [0.04105436, -0.06028581, 0.12204243],
+    [-0.03483987, -0.06210566, 0.13032329],
+    [-0.0133902, 0.21163553, -0.03346758],
+    [0.07170245, 0.11399969, -0.01889817],
+    [-0.08295366, 0.11247234, -0.02370739],
+    [0.01011321, 0.08893734, 0.05040987],
+    [0.12292141, 0.04520509, -0.019046],
+    [-0.11322832, 0.04685326, -0.00847207],
+    [0.2553319, -0.01564902, -0.02294649],
+    [-0.26012748, -0.01436928, -0.03126873],
+    [0.26570925, 0.01269811, -0.00737473],
+    [-0.26910836, 0.00679372, -0.00602676],
+    [0.08669055, -0.01063603, -0.01559429],
+    [-0.0887537, -0.00865157, -0.01010708],
+]
+
+SMPL_PARENTS = [
+    -1,
+    0,
+    0,
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    9,
+    9,
+    12,
+    13,
+    14,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
 ]
 
 
@@ -77,18 +129,10 @@ def load_smpl(smpl_file):
 
 
 def smpl_to_bvh_data(smpl_dict, gender="NEUTRAL", frametime=1 / 60):
-    model = smplx.create(
-        model_path=Path(__file__).parent / "smpl_utils/data/smpl/",
-        model_type="smpl",
-        gender=gender,
-        batch_size=1,
-    )
-    parents = model.parents.detach().cpu().numpy()
-    rest = model()
-    rest_pose = rest.joints.detach().cpu().numpy().squeeze()[:24, :]
+    rest_pose = np.array(SMPL_OFFSETS)
 
     root_offset = rest_pose[0]
-    offsets = rest_pose - rest_pose[parents]
+    offsets = rest_pose  # - rest_pose[parents]
     offsets[0] = root_offset
     offsets *= 100
 
@@ -118,7 +162,7 @@ def smpl_to_bvh_data(smpl_dict, gender="NEUTRAL", frametime=1 / 60):
         "rotations": rotations,
         "positions": positions / 100,  # We want the results in meter convention
         "offsets": offsets / 100,
-        "parents": parents,
+        "parents": SMPL_PARENTS,
         "names": SMPL_JOINTS_NAMES,
         "order": order,
         "frametime": frametime,
@@ -146,13 +190,13 @@ def bvh_data_to_smpl(bvh_data):
     # Reshape rotations to match SMPL format
     rotations = rotations.reshape(rotations.shape[0], -1)
 
-    # Extract root translation and scale it back
-    trans = positions[:, 0] - bvh_data["offsets"][0][None]
+    # Extract root translation
+    trans = positions[:, 0]
 
     # Prepare SMPL dictionary
     smpl_dict = {
         "smpl_poses": rotations,
-        "smpl_trans": trans,
+        "smpl_trans": trans * 100,
         "smpl_scaling": np.array([100]),
     }
 
